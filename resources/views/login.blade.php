c9f3788fc01d1547431b0e74f2636424f4eb243d
cong12q <57665343+cong12q@users.noreply.github.com>
2020-09-30 19:35:01 +0700

    Newfile

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,12 +1,16 @@
-@extends('StudentsLayout.LayoutLogin')
-@section('title','TITLE LOGIN')
-@section('header', 'HEADER LOGIN')
-@section('content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
     <form action="{{ route('post-login') }}" method="POST">
     @csrf
         <input type="text" name="username" placeholder="usename" id="">
         <input type="password" name="password" placeholder="password" id="">
         <button type="submit">Đăng Nhập</button>
     </form>
-@section('footer','FOOTER LOGIN')
-@endsection
+</body>
+</html>
